c69ac01273cc9bfc144783047803db16c9244b82
Wallace Saturnino <wallacesat@live.co.uk>
2024-07-11 01:44:58 -0300

    feat: Install toast lib sooner

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,48 @@
 import { Label } from '@radix-ui/react-label'
+import { CircleCheck, CircleX } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInform = z.infer<typeof signInForm>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInform>()
+
+  async function handleSignIn(data: SignInform) {
+    try {
+      throw new Error('')
+
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Cadastro efetuado 😃', {
+        dismissible: true,
+        icon: <CircleCheck />,
+        description: 'Link de autenticação enviado para o seu e-mail.',
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch (error) {
+      toast.error('Ops! Credenciais inválidas', {
+        dismissible: true,
+        icon: <CircleX />,
+      })
+    }
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -20,13 +58,13 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button className="w-full" type="submit">
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar painel
             </Button>
           </form>
