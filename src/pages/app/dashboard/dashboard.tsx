36b1bcf0dba8fac0d0a126fdbf90b8f7fda1be8b
Wallace Saturnino <wallacesat@live.co.uk>
2024-07-15 23:46:30 -0300

    feat: Implements dashboard header cards

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet-async'
+
+import { DailyOrdersAmountCard } from './daily-orders-amount-card'
+import { MonthlyCanceledOrdersAmountCard } from './monthly-canceld-orders-amount-card'
+import { MonthlyOrdersAmountCard } from './monthly-orders-amount-card'
+import { MonthlyRevenueCard } from './monthly-revenue-card copy'
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className="grid grid-cols-4 gap-4">
+          <MonthlyRevenueCard />
+          <MonthlyOrdersAmountCard />
+          <DailyOrdersAmountCard />
+          <MonthlyCanceledOrdersAmountCard />
+        </div>
+      </div>
+    </>
+  )
+}
