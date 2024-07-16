d1bd575e4c401c9416604ff03f54b47a1668fa62
Wallace Saturnino <wallacesat@live.co.uk>
2024-07-16 01:46:18 -0300

    feat: Implements profile and managed-restaurant services

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,18 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true,
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => {
+      const time = setTimeout(() => {
+        clearTimeout(time)
+        return resolve(null)
+      }, 2000)
+    })
+
+    return config
+  })
+}
