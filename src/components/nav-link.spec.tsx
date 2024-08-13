376a4d74b76f5de43a9cd283a49bb20f30e808e7
Wallace Saturnino <wallacesat@live.co.uk>
2024-08-12 21:39:12 -0300

    test: Writes test to default email for input value on sign-in page

--- src/components/nav-link.spec.tsx
+++ src/components/nav-link.spec.tsx
@@ -21,7 +21,5 @@ describe('NavLInk', () => {
 
     expect(wrapper.getByText('About').dataset.current).toEqual('true')
     expect(wrapper.getByText('Home').dataset.current).toEqual('false')
-
-    wrapper.debug()
   })
 })
