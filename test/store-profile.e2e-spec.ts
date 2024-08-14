1f236143297aa51ffee2d95294afae00b5b2845f
Wallace Saturnino <wallacesat@live.co.uk>
2024-08-13 22:23:29 -0300

    test: Implements test e2e to update-profile modal

---
+++ test/store-profile.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '@playwright/test'
+
+test('Update profile successfully', async ({ page }) => {
+  const newProfileName = 'The Best Pizza'
+
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await page.getByRole('button', { name: 'Pizza Shop' }).click()
+  await page.getByRole('menuitem', { name: 'Perfil da loja' }).click()
+
+  await page.getByLabel('Nome').fill(newProfileName)
+  await page.getByLabel('Description').fill('Changed description')
+
+  await page.getByRole('button', { name: 'Salvar' }).click()
+
+  await page.waitForLoadState('networkidle')
+
+  const toast = page.getByText('Perfil atualizado com sucesso!')
+  expect(toast).toBeVisible()
+
+  await page.getByRole('button', { name: 'Close' }).click()
+  await page.waitForTimeout(300)
+  expect(page.getByRole('button', { name: newProfileName })).toBeVisible()
+})
